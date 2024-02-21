64d2afe7c898faacba4813214317f4dc727fd189
shivi1526 <shivanjali1526@gmail.com>
2024-02-21 10:56:46 +0530

    changes made

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -30,7 +30,7 @@ import Announcementsidebar from "./Components/Admin/Announcementsidebar.jsx";
 import Sidebarannouncement from "./Components/Admin/Sidebarannouncement.jsx";
 import Announcementhomepage from "./Layout/Admin/Announcementhomepage.jsx";
 import EditAnnouncement from "./Layout/Admin/EditAnnouncement.jsx";
-import Createannouncement from "./Layout/Admin/CreateAnnouncement.jsx";
+import Createannouncement from "./Layout/Admin/Createannouncement.jsx";
 import Gradesheethomepage from "./Layout/Admin/Gradesheethomepage.jsx";
 import Entermarksgradesheet from "./Layout/Admin/Entermarksgradesheet.jsx";
 import EditGradeSheetMarks from "./Layout/Admin/EditGradeSheetMarks.jsx";
